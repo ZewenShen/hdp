32215f50c2a19ae36ae5e984afa477f9b0d5b239
Zewen <zewen.public@gmail.com>
2019-08-11 20:29:17 -0400

    Implemented regression with multinomial basis

--- src/blackscholes/utils/Regression.py
+++ src/blackscholes/utils/Regression.py
@@ -3,7 +3,7 @@ import numpy as np
 class Monomial:
 
     def __init__(self, a_vec):
-        self.a_vec = a_vec
+        self.a_vec = np.array(a_vec)
         self.dimension = len(a_vec)
     
     def evaluate(self, input_vec):
@@ -13,10 +13,11 @@ class Monomial:
 class Monomial_Basis:
 
     def __init__(self, chi, dimension):
-        permutations = self._get_all_permutations(chi, dimension)
-        self.monomial = list(map(lambda x: Monomial(x), permutations))
-
-    def _get_all_permutations(self, chi, dimension):
+        permutations = Monomial_Basis._get_all_permutations(chi, dimension)
+        self.monomials = list(map(lambda x: Monomial(x), permutations))
+    
+    @staticmethod
+    def _get_all_permutations(chi, dimension):
         if chi == 0:
             return [[0]*dimension]
         elif dimension == 1:
@@ -24,8 +25,31 @@ class Monomial_Basis:
         else:
             results = []
             for i in range(chi+1):
-                result = map(lambda x: [i] + x, self._get_all_permutations(chi - i, dimension-1))
+                result = map(lambda x: [i] + x, Monomial_Basis._get_all_permutations(chi - i, dimension-1))
                 results += list(result)
             return results
+    
+    def evaluate(self, X):
+        return np.array(list(map(lambda m: m.evaluate(X), self.monomials)))
 
+class Regression:
 
+    def __init__(self, X_mat, Y, chi=2, payoff_func=lambda x: np.sum(x)):
+        assert len(X_mat.shape) == 2, "X in the regression should be a 2d matrix"
+
+        self.dimension = len(X_mat[0])
+        self.basis = Monomial_Basis(chi, self.dimension)
+        
+        index = [i for i in range(len(X_mat)) if payoff_func(X_mat[i]) > 0]
+        target_X, target_Y = X_mat[index], Y[index]
+
+        target_matrix_A = np.array(list(map(lambda x: self.basis.evaluate(x), target_X)))
+        self.coefficients = np.linalg.lstsq(target_matrix_A, target_Y, rcond=None)[0]
+
+    def evaluate(self, X):
+        """
+        X: a numpy array of input data (e.g., asset prices)
+        """
+        assert len(X) == self.dimension, "input vector X doesn't meet the regression dimension"
+        monomial_terms = self.basis.evaluate(X)
+        return np.sum(np.multiply(self.coefficients, monomial_terms))
--- tests/RegressionTest.py
+++ tests/RegressionTest.py
@@ -1,15 +1,26 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../src")
-from blackscholes.utils.Regression import Monomial_Basis
+from blackscholes.utils.Regression import Monomial_Basis, Regression
 import unittest
 import numpy as np
 
 class Test(unittest.TestCase):
     def setUp(self):
-        self.basis = Monomial_Basis(2, 4)
+        pass
     
     def test_get_all_permutations(self):
-        assert self.basis._get_all_permutations(2, 4) == [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 2], [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 2, 0], [0, 1, 0, 0], [0, 1, 0, 1], [0, 1, 1, 0], [0, 2, 0, 0], [1, 0, 0, 0], [1, 0, 0, 1], [1, 0, 1, 0], [1, 1, 0, 0], [2, 0, 0, 0]]
+        assert Monomial_Basis._get_all_permutations(2, 4) == [[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 2], [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 2, 0], [0, 1, 0, 0], [0, 1, 0, 1], [0, 1, 1, 0], [0, 2, 0, 0], [1, 0, 0, 0], [1, 0, 0, 1], [1, 0, 1, 0], [1, 1, 0, 0], [2, 0, 0, 0]]
+    
+    def test_basis_evaluate(self):
+        basis = Monomial_Basis(1, 2)
+        assert (list(map(lambda x: x.a_vec, basis.monomials)) == np.array([[0, 0], [0, 1], [1, 0]])).all()
+        assert (basis.evaluate([2, 3]) == np.array([1, 3, 2])).all()
+
+    def test_regression(self):
+        r = Regression(np.array([[-1, -2], [3, 4], [5, 6], [4, 6], [3, 1]]), np.array([6, 7, 8, 9, 10]), 1)
+        assert abs(r.evaluate([-1, -2]) - 7.878378378378377) < 0.00000000001
+        assert abs(r.evaluate([3, 1]) - 9.594594594594595) < 0.00000000001
+        
 
 
 if __name__ == '__main__':
