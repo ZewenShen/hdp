41ebb74c0b6ee338574697be7048ffdec37f8715
Zewen <zewen.public@gmail.com>
2019-08-12 19:16:33 -0400

    Fundamental structure of Longstaff Schwartz method

--- src/blackscholes/mc/American.py
+++ src/blackscholes/mc/American.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../..")
+from blackscholes.utils.Regression import Regression
 import numpy as np
 
 class American:
@@ -12,3 +15,27 @@ class American:
         self.payoff_func = payoff_func
         self.random_walk = random_walk
     
+    def price(self, path_num=1000):
+        self.simulation_result = self.random_walk.simulate(path_num)
+
+        cashflow_matrix = np.zeros([path_num, self.random_walk.N+1])
+        cur_price = list(map(lambda x: x[:, -1], self.simulation_result))
+        cur_payoff = list(map(self.payoff_func, cur_price))
+        cashflow_matrix[:, self.random_walk.N] = cur_payoff
+
+        for t in range(self.random_walk.N-1, -1, -1):
+            discounted_cashflow = self._get_discounted_cash_flow(t, cashflow_matrix, path_num)
+            # Compute the discounted payoff
+            r = Regression(self.simulation_result[:, :, t], discounted_cashflow, payoff_func=self.payoff_func)
+            cur_price = list(map(lambda x: x[:, t], self.simulation_result))
+            cur_payoff = list(map(self.payoff_func, cur_price))
+
+    def _get_discounted_cash_flow(self, t, cashflow_matrix, path_num):
+        discounted_cashflow = np.zeros(path_num)
+        for j in range(self.random_walk.N, t, -1):
+            for i in range(len(cashflow_matrix[:, j])):
+                if discounted_cashflow[i] != 0: continue
+                cashflow = cashflow_matrix[:, j][i]
+                if cashflow != 0:
+                    discounted_cashflow[i] = cashflow * np.exp((t-j)*self.random_walk.dt*self.random_walk.ir)
+        return discounted_cashflow
---
+++ tests/AmericanTest.py
@@ -0,0 +1,45 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../src")
+from blackscholes.utils.GBM import GBM
+from blackscholes.mc.American import American
+import unittest
+import numpy as np
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        strike = 400
+
+        asset_num = 3
+        init_price_vec = 100*np.ones(asset_num)
+        vol_vec = 0.1*np.ones(asset_num)
+        ir = 0.03
+        dividend_vec = np.zeros(asset_num)
+        corr_mat = np.eye(asset_num)
+        random_walk = GBM(1, 100, init_price_vec, ir, vol_vec, dividend_vec, corr_mat)
+
+        def test_payoff(*l):
+            return max(strike - np.sum(l), 0)
+
+        self.opt1 = American(test_payoff, random_walk)
+        
+    def test_price1d(self):
+        simulation_result = self.opt1.random_walk.simulate(2)
+        print(np.array(simulation_result)[:,:,1])
+        #approx_put = self.opt1.price(50)
+
+    def test_get_discounted_cash_flow(self):
+        np.random.seed(1)
+        random_walk = GBM(3, 3, np.ones(1), 0.03, np.ones(1), np.zeros(1), np.eye(1))
+        def test_payoff(*l):
+            return max(3 - np.sum(l), 0)
+        opt = American(test_payoff, random_walk)
+        cashflow_matrix = np.array([[0, 0, 0, 3], [0, 0, 0, 0], [0, 0, 0, 2]])
+        discounted = opt._get_discounted_cash_flow(2, cashflow_matrix, 3)
+        assert sum(abs(discounted - np.array([2.9113366, 0, 1.94089107]))) < 0.00000001
+
+        cashflow_matrix2 = np.array([[0, 0, 3, 0], [0, 0, 0, 0], [0, 0, 0, 2]])
+        discounted2 = opt._get_discounted_cash_flow(0, cashflow_matrix2, 3)
+        assert sum(abs(discounted2 - np.array([2.8252936, 0, 1.82786237]))) < 0.00000001
+
+if __name__ == '__main__':
+    unittest.main()
