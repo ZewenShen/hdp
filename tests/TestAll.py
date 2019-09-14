2500edc3c3870fb58f82ad4fbc238756c146b320
Zewen <zewen.public@gmail.com>
2019-09-14 14:05:39 -0400

    Update tests' names

--- tests/TestAll.py
+++ tests/TestAll.py
@@ -1,8 +1,19 @@
 import os
 directory = os.path.dirname(os.path.abspath(__file__))
-os.system('python {}/EuroTest.py'.format(directory))
-os.system('python {}/AmericanTest.py'.format(directory))
+
+print("-------------------- TestAll Started --------------------")
+
+print("-------------------- Running MCEuroTest --------------------")
+os.system('python {}/MCEuroTest.py'.format(directory))
+print("-------------------- Running MCAmericanTest --------------------")
+os.system('python {}/MCAmericanTest.py'.format(directory))
+print("-------------------- Running PCATest --------------------")
 os.system('python {}/PCATest.py'.format(directory))
+print("-------------------- Running RegressionTest --------------------")
 os.system('python {}/RegressionTest.py'.format(directory))
+print("-------------------- Running ParabolicTest --------------------")
 os.system('python {}/ParabolicTest.py'.format(directory))
+print("-------------------- Running VannilaNetworkTest --------------------")
 os.system('python {}/VannilaNetworkTest.py'.format(directory))
+
+print("-------------------- TestAll Finished --------------------")
