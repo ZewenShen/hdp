070a90c5e917603607c720f82c56be1ae15923f7
Zewen <zewen.public@gmail.com>
2019-09-17 23:00:39 -0400

    Move Domain to the general utils package

--- tests/ParabolicTest.py
+++ tests/ParabolicTest.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../src")
 from blackscholes.pde.Parabolic import Solver1d, Coef2d, Solver2d
 from blackscholes.pde.Euro import Euro1d
 from blackscholes.utils.Analytical import Analytical_Sol
-from blackscholes.utils.Domain import Domain1d, Domain2d
+from utils.Domain import Domain1d, Domain2d
 from blackscholes.utils.Type import CallPutType
 import unittest
 import numpy as np
