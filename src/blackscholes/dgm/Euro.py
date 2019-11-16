1dd101ad13375924880c5d70adb69ce192a10be2
Zewen <zewen.public@gmail.com>
2019-11-15 23:03:22 -0500

    DGM: Add Nd European model (version 1)

--- src/blackscholes/dgm/Euro.py
+++ src/blackscholes/dgm/Euro.py
@@ -2,7 +2,7 @@ import sys, os, time
 DIR_LOC = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(DIR_LOC+"/..")
 from blackscholes.dgm.DGMNet import DGMNet
-from utils.Domain import Sampler1d
+from utils.Domain import Sampler1d, SamplerNd
 import utils.Pickle as pickle
 import tensorflow as tf
 import numpy as np
@@ -16,15 +16,77 @@ class Euro:
         """
         self.payoff_func = payoff_func
         self.domain = domain
+        self.dim = domain.dim
         self.vol_vec = vol_vec
         self.ir = ir
         self.dividend_vec = dividend_vec
-        self.corr_mat = corr_mat
-        self.sampler = sampler
+        self.cov_mat = (self.vol_vec[np.newaxis].T @ self.vol_vec[np.newaxis]) * corr_mat
+        self.sampler = sampler if sampler is not None else SamplerNd(domain)
 
     def run(self,  n_samples, steps_per_sample, n_layers=3, layer_width=50, n_interior=1000, n_boundary=100, n_terminal=100, saved_name=None):
-        pass
-    
+        model = DGMNet(n_layers, layer_width, input_dim=self.dim)
+        self.model = model
+        S_interior_tnsr = tf.placeholder(tf.float32, [None, self.dim])
+        t_interior_tnsr = tf.placeholder(tf.float32, [None, 1])
+        S_terminal_tnsr = tf.placeholder(tf.float32, [None, self.dim])
+        t_terminal_tnsr = tf.placeholder(tf.float32, [None, 1])
+        L1_tnsr, L3_tnsr = self.loss_func(model, S_interior_tnsr, t_interior_tnsr,\
+            S_terminal_tnsr, t_terminal_tnsr)
+        loss_tnsr = L1_tnsr + L3_tnsr
+
+        optimizer = tf.train.AdamOptimizer().minimize(loss_tnsr)
+
+        self.loss_vec, self.L1_vec, self.L3_vec = [], [], []
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(n_samples):
+                S_interior, t_interior, S_terminal, t_terminal = \
+                    self.sampler.run(n_interior, n_terminal)
+                for _ in range(steps_per_sample):
+                    loss, L1, L3, _ = sess.run([loss_tnsr, L1_tnsr, L3_tnsr, optimizer],\
+                        feed_dict={S_interior_tnsr: S_interior, t_interior_tnsr: t_interior,\
+                                   S_terminal_tnsr: S_terminal, t_terminal_tnsr: t_terminal})
+                self.loss_vec.append(loss); self.L1_vec.append(L1); self.L3_vec.append(L3)
+                print("Iteration {}: Loss: {}; L1: {}; L3: {}".format(i, loss, L1, L3))
+
+
+    def loss_func(self, model, S_interior, t_interior, S_terminal, t_terminal):
+        ''' Compute total loss for training.
+        
+        Args:
+            model:      DGMNet model object
+            t_interior: sampled time points in the interior of the function's domain
+            S_interior: sampled space points in the interior of the function's domain
+            t_terminal: sampled time points at terminal point (vector of terminal times)
+            S_terminal: sampled space points at terminal time
+        ''' 
+        # Loss term #1: PDE
+        # compute function value and derivatives at current sampled points
+        V = model(S_interior, t_interior)
+        V_t = tf.gradients(V, t_interior)[0]
+        V_s = tf.gradients(V, S_interior)[0]
+        V_ss = tf.hessians(V, S_interior)[0]
+        V_ss = tf.reduce_sum(V_ss, axis=2)
+
+        cov_Vss = tf.multiply(V_ss, self.cov_mat)
+        sec_ords = tf.map_fn(lambda x: tf.reduce_sum(tf.tensordot(S_interior, x, 1) * S_interior, axis=1) / 2,\
+                    cov_Vss)
+        sec_ord = tf.reduce_sum(sec_ords, axis=1)
+        first_ord = tf.reduce_sum(tf.multiply(V_s, S_interior), axis=1)
+        diff_V = V_t + sec_ord + tf.multiply(self.ir - self.dividend_vec, first_ord) - self.ir * V
+
+        # compute average L2-norm of differential operator
+        L1 = tf.reduce_mean(tf.square(diff_V))
+        
+        
+        # Loss term #3: initial/terminal condition
+        target_payoff = tf.nn.relu(self.payoff_func(S_terminal))
+        fitted_payoff = model(S_terminal, t_terminal)
+        
+        L3 = tf.reduce_mean(tf.square(fitted_payoff - target_payoff))
+
+        return L1, L3
+
 class Euro1d:
 
     def __init__(self, domain, vol, ir, dividend, strike, cp_type, sampler=None):
@@ -49,12 +111,12 @@ class Euro1d:
 
         model = DGMNet(n_layers, layer_width, input_dim=1)
         self.model = model
-        S_interior_tnsr = tf.placeholder(tf.float32, [None,1])
-        t_interior_tnsr = tf.placeholder(tf.float32, [None,1])
-        S_boundary_tnsr = tf.placeholder(tf.float32, [None,1])
-        t_boundary_tnsr = tf.placeholder(tf.float32, [None,1])
-        S_terminal_tnsr = tf.placeholder(tf.float32, [None,1])
-        t_terminal_tnsr = tf.placeholder(tf.float32, [None,1])
+        S_interior_tnsr = tf.placeholder(tf.float32, [None, 1])
+        t_interior_tnsr = tf.placeholder(tf.float32, [None, 1])
+        S_boundary_tnsr = tf.placeholder(tf.float32, [None, 1])
+        t_boundary_tnsr = tf.placeholder(tf.float32, [None, 1])
+        S_terminal_tnsr = tf.placeholder(tf.float32, [None, 1])
+        t_terminal_tnsr = tf.placeholder(tf.float32, [None, 1])
         L1_tnsr, L2_tnsr, L3_tnsr = self.loss_func(model, S_interior_tnsr, t_interior_tnsr,\
             S_boundary_tnsr, t_boundary_tnsr, S_terminal_tnsr, t_terminal_tnsr)
         loss_tnsr = L1_tnsr + L2_tnsr + L3_tnsr
--- tests/DGMEuroTrainingTest.py
+++ tests/DGMEuroTrainingTest.py
@@ -1,13 +1,24 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../src")
-from blackscholes.dgm.Euro import Euro1d
-from utils.Domain import Domain1d, Sampler1dBoundary2Center
+from blackscholes.dgm.Euro import Euro1d, Euro
+from utils.Domain import Domain1d, Sampler1dBoundary2Center, DomainNd, SamplerNd
 from blackscholes.utils.Type import CallPutType
 import unittest
 import numpy as np
 import tensorflow as tf
 
 class Test(unittest.TestCase):
+
+    def test_euro2d(self):
+        dim = 2
+        T = 1
+        domain = DomainNd(np.array([[0, 3], [0, 3]]), T)
+        vol_vec, ir, dividend_vec, strike = 0.1*np.ones(dim), 0.03, 0.01*np.ones(dim), 1
+        payoff_func = lambda x: np.maximum(np.sum(x, axis=1) - strike, 0)
+        corr_mat = 0.25 * np.ones((dim, dim))
+        np.fill_diagonal(corr_mat, 1)
+        solver = Euro(payoff_func, domain, vol_vec, ir, dividend_vec, corr_mat)
+        solver.run(n_samples=2000, steps_per_sample=10)
 
     def test_euro1d(self):
         tf.random.set_random_seed(4)
