9744ff30d8fd8ce101c198dcaa4d0d25fafc01ed
Zewen <zewen.public@gmail.com>
2019-11-28 21:09:52 -0500

    Update Experiment.py

--- src/utils/Experiment.py
+++ src/utils/Experiment.py
@@ -9,6 +9,7 @@ class ExperimentResult:
         self.N = N
         self.results = np.array(results)
         self.times = times
+        self.analytical_sol = analytical_sol
         if errors:
             self.errors = np.array(errors)
             self.relative_errors = self.errors / analytical_sol
@@ -18,20 +19,22 @@ class ExperimentResult:
             self.orders = np.log2((errors[:-2] - errors[1:-1]) / (errors[1:-1] - errors[2:]))
 
     def __str__(self):
-        result = "N    rel_err    orders    times\n"
+        result = "N    approx    rel_err    orders    times\n"
         if self.errors is not None:
-            result += "{:d}    {:.1e}    NA    {:.1f}\n".format(self.N[0], self.relative_errors[0], self.times[0])
+            result += "{:d}    {:.3f}    {:.1e}    NA    {:.2f}\n".format(self.N[0], self.results[0],\
+                    self.relative_errors[0], self.times[0])
             for i in range(1, len(self.N)):
-                result += "{:d}    {:.1e}    {:.1f}    {:.1f}\n".format(\
-                    self.N[i], self.relative_errors[i], self.orders[i-1], self.times[i])
+                result += "{:d}    {:.3f}    {:.1e}    {:.1f}    {:.2f}\n".format(\
+                    self.N[i], self.results[i], self.relative_errors[i], self.orders[i-1], self.times[i])
         else:
-            result += "{:d}    {:.1e}    NA    {:.1f}\n".format(\
-                    self.N[0], self.relative_errors[0], self.times[0])
-            result += "{:d}    {:.1e}    NA    {:.1f}\n".format(\
-                    self.N[1], self.relative_errors[1], self.times[1])
+            result += "{:d}    {:.1e}    NA    {:.2f}\n".format(\
+                    self.N[0], self.results[0], self.times[0])
+            result += "{:d}    {:.1e}    NA    {:.2f}\n".format(\
+                    self.N[1], self.results[1], self.times[1])
             for i in range(1, len(self.N)):
-                result += "{:d}    {:.1e}    {:.1f}    {:.1f}\n".format(\
-                    self.N[i], self.relative_errors[i], self.orders[i-2], self.times[i])
+                result += "{:d}    {:.1e}    {:.1f}    {:.2f}\n".format(\
+                    self.N[i], self.results[i], self.orders[i-2], self.times[i])
+        result += "Analytical Sol: " + "{:.6f}".format(self.analytical_sol) + '\n'
         return result
 
 def FFTConvExperiment(analytical_sol, n_start, n_end, FFT_Euro, L_multiplier=30):
