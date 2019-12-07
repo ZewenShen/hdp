a6c2958f4066f15a1c8196e0af2bfaba1a1b156b
Zewen <zewen.public@gmail.com>
2019-12-07 10:28:07 -0500

    Update MCEuroExperiment.py

--- experiments/MCEuroExperiment.py
+++ experiments/MCEuroExperiment.py
@@ -49,6 +49,29 @@ class Test(unittest.TestCase):
         hdpPickle.dump(result, 'MCEuro_6d.pickle')
         print(result)
     
+    
+    def test_conv_rate_6d_control_sobol(self):
+        dim = 6
+        T = 1
+        strike = 40
+        init_price_vec = np.full(dim, 40)
+        vol = 0.2
+        ir = 0.06
+        dividend = 0.04
+        corr = 0.25
+        vol_vec = np.full(dim, vol)
+        dividend_vec = np.full(dim, dividend)
+        corr_mat = np.full((dim, dim), corr)
+        np.fill_diagonal(corr_mat, 1)
+        payoff_func = lambda x: np.maximum(strike - np.mean(x, axis=1), np.zeros(len(x)))
+        random_walk = GBM(T, 400, init_price_vec, ir, vol_vec, dividend_vec, corr_mat)
+        opt = Euro(payoff_func, random_walk)
+        analy = 0
+        np.random.seed(1)
+        result = MCEuroExperiment(analy, 20, 27, opt, "V8")
+        hdpPickle.dump(result, 'MCEuro_6d_control_sobol.pickle')
+        print(result)
+
     def test_conv_rate_6d_control(self):
         dim = 6
         T = 1
